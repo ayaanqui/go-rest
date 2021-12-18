02110eae66717a1685cea430c51216f3fb97b51c
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-17 21:47:01 -0600

    create server with simple response

--- src/main.go
+++ src/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/ayaanqui/go-rest-server/src/types"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(rw).Encode(types.Response{Message: "Hello world"})
+	})
+
+	const port = "3001"
+	fmt.Printf("Starting server at port %s...\n", port)
+	if err := http.ListenAndServe(":" + port, nil); err != nil {
+		log.Fatalf("Server already started on port %s\n\n", port)
+		log.Fatal(err)
+	}
 }
---
+++ src/types/json.go
@@ -0,0 +1,5 @@
+package types
+
+type Response struct {
+	Message string `json:"message"`
+}
