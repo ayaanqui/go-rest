b1b17bc61bcd0d322c49b79bb6de305c8ec7e57f
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-19 14:52:14 -0600

    create base handler with db connection

---
+++ src/types/app.go
@@ -0,0 +1,15 @@
+package types
+
+import "database/sql"
+
+type AppBase struct {
+	DB *sql.DB
+}
+
+type IAppBase interface {
+	NewBaseHandler(conn *sql.DB)
+}
+
+func (app *AppBase) NewBaseHandler(conn *sql.DB) {
+	app.DB = conn
+}
