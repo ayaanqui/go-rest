ed5490338ddbb6257281b18d00e1f7ce4aa5e3ff
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-19 13:15:00 -0600

    handle routes with method handler middleware

--- src/main.go
+++ src/main.go
@@ -6,12 +6,13 @@ import (
 	"net/http"
 
 	"github.com/ayaanqui/go-rest-server/src/routes"
+	"github.com/ayaanqui/go-rest-server/src/utils"
 )
 
 func main() {
 	server := http.NewServeMux()
-	server.HandleFunc("/", routes.Home)
-	server.HandleFunc("/post", routes.Post)
+	server.Handle("/", utils.HandleGet(http.HandlerFunc(routes.Home)))
+	server.Handle("/post", utils.HandlePost(http.HandlerFunc(routes.Post)))
 
 	const port = "3001"
 	fmt.Printf("Starting server at port %s...\n", port)
