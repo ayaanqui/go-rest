46ca0f57c2b69e51b0f15c27ae9fa79deba1f819
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-19 23:03:02 -0600

    add post table

--- src/routes/app.go
+++ src/routes/app.go
@@ -16,17 +16,31 @@ type IAppBase interface {
 func (app *AppBase) NewBaseHandler(conn *sql.DB) {
 	app.DB = conn
 
-	_, err := conn.Exec(`
-		CREATE TABLE IF NOT EXISTS home (
-			id BINARY(16) NOT NULL DEFAULT(
-				UUID_TO_BIN(UUID())
-			) PRIMARY KEY, 
-			message TEXT NOT NULL,
-			date DATETIME NOT NULL DEFAULT(NOW())
-		)
-	`)
-	if err != nil {
+	home := `CREATE TABLE IF NOT EXISTS home (
+		id BINARY(16) NOT NULL DEFAULT(
+			UUID_TO_BIN(UUID())
+		) UNIQUE PRIMARY KEY, 
+		message TEXT NOT NULL,
+		date DATETIME NOT NULL DEFAULT(NOW())
+	)`
+	if _, err := conn.Exec(home); err != nil {
 		log.Fatal("Could not execute CREATE query for home table.\n", err)
+		return
+	}
+
+	post := `CREATE TABLE IF NOT EXISTS post (
+		id BINARY(16) NOT NULL DEFAULT(
+			UUID_TO_BIN(UUID())
+		) UNIQUE PRIMARY KEY, 
+		title VARCHAR(255) NOT NULL,
+		slug VARCHAR(255) NOT NULL UNIQUE PRIMARY KEY,
+		content TEXT NOT NULL,
+		summary TEXT NOT NULL,
+		date DATETIME NOT NULL DEFAULT(NOW()),
+		updated DATETIME NOT NULL DEFAULT(NOW())
+	)`
+	if _, err := conn.Exec(post); err != nil {
+		log.Fatal("Could not execute CREATE query for post table.\n", err)
 		return
 	}
 }
