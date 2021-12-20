4cabce4c96d7cc9b5954ed11a363d86ab9c438f6
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-19 21:02:01 -0600

    initialize tables if not already

--- src/routes/app.go
+++ src/routes/app.go
@@ -1,6 +1,9 @@
 package routes
 
-import "database/sql"
+import (
+	"database/sql"
+	"log"
+)
 
 type AppBase struct {
 	DB *sql.DB
@@ -12,4 +15,18 @@ type IAppBase interface {
 
 func (app *AppBase) NewBaseHandler(conn *sql.DB) {
 	app.DB = conn
+
+	_, err := conn.Exec(`
+		CREATE TABLE IF NOT EXISTS home (
+			id BINARY(16) NOT NULL DEFAULT(
+				UUID_TO_BIN(UUID())
+			) PRIMARY KEY, 
+			message TEXT NOT NULL,
+			date DATETIME NOT NULL DEFAULT(NOW())
+		)
+	`)
+	if err != nil {
+		log.Fatal("Could not execute CREATE query for home table.\n", err)
+		return
+	}
 }
