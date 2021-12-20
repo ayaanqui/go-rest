db06156e1204389ea52d8ef6d1a2b1ef0bfd3903
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-19 20:22:16 -0600

    error checking for json response function

--- src/utils/response.go
+++ src/utils/response.go
@@ -3,8 +3,16 @@ package utils
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/ayaanqui/go-rest-server/src/types"
 )
 
 func JsonResponse(w http.ResponseWriter, data interface{}) {
-	json.NewEncoder(w).Encode(data)
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		json.NewEncoder(w).Encode(
+			types.Response{
+				Message: "Could not parse response",
+			},
+		)
+	}
 }
