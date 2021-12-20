ef12376224b1b635d1c87eead46491fe4ce5024c
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-19 19:57:57 -0600

    establish DB connection

--- src/main.go
+++ src/main.go
@@ -6,10 +6,20 @@ import (
 	"net/http"
 
 	"github.com/ayaanqui/go-rest-server/src/routes"
+	"github.com/ayaanqui/go-rest-server/src/types"
 	"github.com/ayaanqui/go-rest-server/src/utils"
 )
 
 func main() {
+	conn, err := utils.DbConnect()
+	if err != nil {
+		log.Fatal("Could not connect to database.\n", err)
+		return
+	}
+	defer conn.Close()
+	app := types.AppBase{}
+	app.NewBaseHandler(conn)
+
 	server := http.NewServeMux()
 	server.Handle("/", utils.HandleGet(http.HandlerFunc(routes.Home)))
 	server.Handle("/post", utils.HandlePost(http.HandlerFunc(routes.Post)))
