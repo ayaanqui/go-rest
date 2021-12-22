1adda59b95bc0455c4612c53fd3529aa110a8a2f
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-21 18:56:28 -0600

    add gorm.Model schema

--- src/types/schema.go
+++ src/types/schema.go
@@ -1,17 +1,35 @@
 package types
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Base struct {
+	gorm.Model
+	ID uuid.UUID `gorm:"type:char(36); primary key" json:"id"`
+	CreatedAt time.Time `gorm:"not null" json:"created_at"`
+	UpdatedAt time.Time `gorm:"not null" json:"updated_at"`
+}
+
+func (base *Base) BeforeCreate(tx *gorm.DB) error {
+	base.ID = uuid.New()
+	base.CreatedAt = time.Now()
+	base.UpdatedAt = time.Now()
+	return nil
+}
+
 type Post struct {
-	Id string `json:"id"`
-	Title string `json:"title"`
-	Slug string `json:"slug"`
-	Content string `json:"content"`
-	Summary string `json:"summary"`
-	Date string `json:"date"`
-	Updated string `json:"updated"`
+	Base
+	Title string `gorm:"not null" json:"title"`
+	Slug string `gorm:"not null" json:"slug"`
+	Content string `gorm:"type:text; not null" json:"content"`
+	Summary string `gorm:"not null; default: ''" json:"summary"`
 }
 
 type Home struct {
-	Id string `json:"id"`
-	Message string `json:"message"`
-	Date string `json:"date"`
+	Base
+	Message string `gorm:"type:text; not null" json:"message"`
 }
