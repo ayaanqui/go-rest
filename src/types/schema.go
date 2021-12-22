b9a89ef0fbc7b57bbd9d54a86e18318216801ca9
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-21 19:41:23 -0600

    remove omitempty tag

--- src/types/schema.go
+++ src/types/schema.go
@@ -9,9 +9,9 @@ import (
 
 type Base struct {
 	gorm.Model
-	ID uuid.UUID `gorm:"type:char(36); primary key" json:"id,omitempty"`
-	CreatedAt time.Time `gorm:"not null" json:"created_at,omitempty"`
-	UpdatedAt time.Time `gorm:"not null" json:"updated_at,omitempty"`
+	ID uuid.UUID `gorm:"type:char(36); primary key" json:"id"`
+	CreatedAt time.Time `gorm:"not null" json:"created_at"`
+	UpdatedAt time.Time `gorm:"not null" json:"updated_at"`
 }
 
 func (base *Base) BeforeCreate(tx *gorm.DB) error {
@@ -23,13 +23,13 @@ func (base *Base) BeforeCreate(tx *gorm.DB) error {
 
 type Post struct {
 	Base
-	Title string `gorm:"not null" json:"title,omitempty"`
-	Slug string `gorm:"not null" json:"slug,omitempty"`
-	Content string `gorm:"type:text; not null" json:"content,omitempty"`
-	Summary string `gorm:"not null; default: ''" json:"summary,omitempty"`
+	Title string `gorm:"not null" json:"title"`
+	Slug string `gorm:"not null" json:"slug"`
+	Content string `gorm:"type:text; not null" json:"content"`
+	Summary string `gorm:"not null; default: ''" json:"summary"`
 }
 
 type Home struct {
 	Base
-	Message string `gorm:"type:text; not null" json:"message,omitempty"`
+	Message string `gorm:"type:text; not null" json:"message"`
 }
