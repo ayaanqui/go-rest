d94b1bd7a8ed4959c7f1b076e6daa5c8b11bc616
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-01-17 00:06:31 -0600

    refactor: move parser functions from routes middlewares to utils

--- src/routes/middlewares.go
+++ src/routes/middlewares.go
@@ -2,13 +2,10 @@ package routes
 
 import (
 	"context"
-	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/ayaanqui/go-rest-server/src/types"
 	"github.com/ayaanqui/go-rest-server/src/utils"
-	"github.com/golang-jwt/jwt"
 	"github.com/google/uuid"
 )
 
@@ -18,7 +15,7 @@ func UseJwtAuth(app *AppBase, next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authorization := r.Header.Get("Authorization")
 		// Parse bearer token
-		raw_token, err := GetBearerToken(authorization)
+		raw_token, err := utils.GetBearerToken(authorization)
 		if err != nil {
 			w.WriteHeader(401)
 			utils.JsonResponse(w, types.Response{Message: AUTH_REQ})
@@ -26,7 +23,7 @@ func UseJwtAuth(app *AppBase, next http.Handler) http.Handler {
 		}
 
 		// Parse JWT
-		claims, err := GetJwtClaims(raw_token, app.Tokens.JwtKey)
+		claims, err := utils.GetJwtClaims(raw_token, app.Tokens.JwtKey)
 		if err != nil {
 			w.WriteHeader(401)
 			utils.JsonResponse(w, types.Response{Message: AUTH_REQ})
@@ -54,34 +51,4 @@ func UseJwtAuth(app *AppBase, next http.Handler) http.Handler {
 		// Serve handler with updated request
 		next.ServeHTTP(w, r)
 	})
-}
-
-// Given a bearer token ("Bearer <TOKEN>") returns the token or an error if parsing was unsuccessful
-func GetBearerToken(authorization string) (string, error) {
-	parsed_authorization := strings.Split(authorization, " ")
-	if parsed_authorization[0] != "Bearer" || len(parsed_authorization) < 2 {
-		return "", fmt.Errorf("could not parse bearer token")
-	}
-	token := parsed_authorization[1]
-	return token, nil
-}
-
-// Given a raw jwt token and an encryption key return the mapped jwt claims or an error
-func GetJwtClaims(jwt_token string, key string) (jwt.MapClaims, error) {
-	token, token_err := jwt.Parse(jwt_token, func(t *jwt.Token) (interface{}, error) {
-		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("invalid signing method")
-		}
-		return []byte(key), nil
-	})
-	if token_err != nil {
-		return nil, fmt.Errorf("could not parse jwt token")
-	}
-	
-	// Get claims stored in parsed JWT token
-	claims, ok := token.Claims.(jwt.MapClaims)
-	if !ok {
-		return nil, fmt.Errorf("could not fetch jwt claims")
-	}
-	return claims, nil
 }
--- src/tests/utils_test.go
+++ src/tests/utils_test.go
@@ -3,13 +3,13 @@ package tests
 import (
 	"testing"
 
-	"github.com/ayaanqui/go-rest-server/src/routes"
+	"github.com/ayaanqui/go-rest-server/src/utils"
 )
 
 func TestGetBearerToken(t *testing.T) {
     // Test properly formatted authorization field
 	{
-        token1, err := routes.GetBearerToken("Bearer my_token")
+        token1, err := utils.GetBearerToken("Bearer my_token")
         if token1 != "my_token" && err == nil {
             t.Errorf("did not parse bearer token properly")
         }
@@ -17,7 +17,7 @@ func TestGetBearerToken(t *testing.T) {
 
     // Test with empty bearer token
     {
-        _, err := routes.GetBearerToken("Bearer")
+        _, err := utils.GetBearerToken("Bearer")
         if err == nil {
             t.Errorf("did not parse bearer token properly")
         }
@@ -25,7 +25,7 @@ func TestGetBearerToken(t *testing.T) {
 
     // Test with no value
     {
-        _, err := routes.GetBearerToken("")
+        _, err := utils.GetBearerToken("")
         if err == nil {
             t.Errorf("did not parse bearer token properly")
         }
@@ -38,7 +38,7 @@ func TestGetJwtClaims(t *testing.T) {
         key := "abcd123"
         jwt := "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE2NDIzOTc5OTksImV4cCI6MTY3MzkzMzk5OSwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsImVtYWlsIjoiZXhhbXBsZUBlbWFpbC5jb20iLCJ1c2VybmFtZSI6ImV4YW1wbGUifQ.fBJbtYyIJuHA6Ip8OlQuVmDrHlIhtSAlx7S3lUBK_qM"
 
-        claims_map, err := routes.GetJwtClaims(jwt, key)
+        claims_map, err := utils.GetJwtClaims(jwt, key)
         username := claims_map["username"]
         email := claims_map["email"]
         if err != nil || email != "example@email.com" || username != "example" {
@@ -51,7 +51,7 @@ func TestGetJwtClaims(t *testing.T) {
         key := "incorrect key"
         jwt := "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE2NDIzOTc5OTksImV4cCI6MTY3MzkzMzk5OSwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsImVtYWlsIjoiZXhhbXBsZUBlbWFpbC5jb20iLCJ1c2VybmFtZSI6ImV4YW1wbGUifQ.fBJbtYyIJuHA6Ip8OlQuVmDrHlIhtSAlx7S3lUBK_qM"
 
-        if _, err := routes.GetJwtClaims(jwt, key); err == nil {
+        if _, err := utils.GetJwtClaims(jwt, key); err == nil {
             t.Fail()
         }
     }
@@ -61,7 +61,7 @@ func TestGetJwtClaims(t *testing.T) {
         key := "q34859t8jsvdh1"
         jwt := "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE2NDIzOTc5OTksImV4cCI6MTY3MzkzMzk5OSwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsImBtYWlsIjoiZXhhbXBsZUBlbWFpbC5jb20iLCJ1c2VybmFtZSI6ImV4YW1wbGUifQ.Ohw7jfG65CzgiTB-DZMVoKl67APTeJrwrmHd3Ex9KX0"
 
-        if _, err := routes.GetJwtClaims(jwt, key); err == nil {
+        if _, err := utils.GetJwtClaims(jwt, key); err == nil {
             t.Fail()
         }
     }
