9eb8f147b0fae6c415463c661388f8fca6021e47
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-03-06 21:35:20 -0600

    refactor: move oauth provider setup logic to `oauth_provider.go`

--- src/app/routes.go
+++ src/app/routes.go
@@ -4,15 +4,10 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/markbates/goth"
-	"github.com/markbates/goth/providers/twitter"
 )
 
 // Create routes given a gorilla/mux router instance
 func (app *AppBase) CreateRoutes(router *mux.Router) *AppBase {
-	// Initialize goth
-	goth.UseProviders(twitter.New(app.Tokens.Twitter.ApiKey, app.Tokens.Twitter.ApiKeySecret, "http://localhost:3001/auth/twitter/callback"))
-
 	router.HandleFunc("/", app.Home).Methods("GET")
 	router.HandleFunc("/posts", app.CreatePost).Methods("POST")
 	router.HandleFunc("/posts", app.GetPosts).Methods("GET")
